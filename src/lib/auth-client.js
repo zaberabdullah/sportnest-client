5dad63fee1b7b7b6420f837b35c5273a7d23da41
zaberabdullah <zaberabdullah120@gmail.com>
2026-05-22 06:12:04 +0600

    remove server code

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,12 +1,8 @@
-import { createAuthClient } from "better-auth/react";
+import { createAuthClient } from "better-auth/react"
 
 export const authClient = createAuthClient({
-  baseURL: "http://localhost:3000", 
-});
+  baseURL: process.env.NEXT_PUBLIC_API_URL  
+  // https://sportnest-server-srdb.onrender.com
+})
 
-export const {
-  signIn,
-  signOut,
-  signUp,
-  useSession,
-} = authClient;
+export const { signIn, signUp, signOut, useSession } = authClient
